caaf8f9608045ec0a8e6b241dd39fc4471eba37b
arman-engineering <armaninformatika2021@gmail.com>
2022-09-24 17:51:04 +0700

    first commit

--- src/tugas1/Ayam.java
+++ src/tugas1/Ayam.java
@@ -5,12 +5,12 @@ public class Ayam {
     String jenis, warna;
 
     //behavior atau method-2 nya
-    void makan(String jenisAyam, String warnaAyam) { //jenisAyam dan warnaAyam itu adalah sebuah parameter
+    void makan(String jenis, String warna) { //variable dalam kurung adalah parameter
         
-        jenis = jenisAyam; //variable jenis kita beri value dari nilai jenisAyam
-        warna = warnaAyam; //variable warna kita beri value dari nilai warnaAyam
+        this.jenis = jenis; 
+        this.warna = warna; 
         
         //cetak apa yang mau di cetak
-        System.out.println("Ayam Jenis " + jenis + ", Warna " + warna + " Sedang Makan");
+        System.out.println("Ayam Jenis " + this.jenis + ", Warna " + this.warna + " Sedang Makan");
     }
 }
--- src/tugas1/Tugas1.java
+++ src/tugas1/Tugas1.java
@@ -1,11 +1,13 @@
 package tugas1;
 public class Tugas1 {
     public static void main(String[] args) {
+        
         //membuat object dari class yang sudah ada
         Ayam ayam1 = new Ayam(); //kata ayam1 adalah objectnya
-        Ayam ayam2 = new Ayam(); //kata ayam2 juga adalah object
+        
         //pemanggilan object
         ayam1.makan("Betina", "Hitam"); //"Betina, Hitam" itu adalah nilai dari parameter yg ada pada void makan()
-        ayam2.makan("Jantan", "Merah"); //"Jantan, Merah" itu adalah nilai dari parameter yg ada pada void makan()
+        
+        //lakukan sampai 10 object
     }
 }
